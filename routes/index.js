585f8fc90f675414c73f7df9e3cbc853986c5233
Hakizimana Eric Pisco <hakizericpisco@gmail.com>
2023-12-06 12:44:20 +0000

    index

--- routes/index.js
+++ routes/index.js
@@ -1,25 +1,52 @@
-import express from 'express';
+import { Router } from 'express';
 import AppController from '../controllers/AppController';
 import UsersController from '../controllers/UsersController';
 import AuthController from '../controllers/AuthController';
 import FilesController from '../controllers/FilesController';
+import UtilController from '../controllers/UtilController';
 
-const router = express.Router();
+const router = Router();
 
+// requireAuth
+router.use((request, response, next) => {
+  const paths = ['/connect'];
+  if (!paths.includes(request.path)) {
+    next();
+  } else if (!request.headers.authorization) {
+    response.status(401).json({ error: 'Unauthorized' }).end();
+  } else {
+    next();
+  }
+});
+
+// requireSession
+router.use((request, response, next) => {
+  const paths = ['/disconnect', '/users/me', '/files'];
+  if (!paths.includes(request.path)) {
+    next();
+  } else if (!request.headers['x-token']) {
+    response.status(401).json({ error: 'Unauthorized' }).end();
+  } else {
+    next();
+  }
+});
+
+// ------------------- Users & misc ----------------------------
+router.get('/users/me', UtilController.token, UsersController.getMe);
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
-
 router.post('/users', UsersController.postNew);
-router.get('/users/me', UsersController.getMe);
-
 router.get('/connect', AuthController.getConnect);
-router.get('/disconnect', AuthController.getDisconnect);
+router.get('/disconnect', UtilController.token, AuthController.getDisconnect);
 
-router.post('/files', FilesController.postUpload);
-router.get('/files/:id', FilesController.getShow);
-router.get('/files', FilesController.getIndex);
-router.put('/files/:id/publish', FilesController.putPublish);
-router.put('/files/:id/publish', FilesController.putUnpublish);
-router.get('/files/:id/data', FilesController.getFile);
+// ---------------------- files --------------------------------
+router.post('/files', UtilController.token, FilesController.postUpload);
+
+router.get('/files/:id', UtilController.token, FilesController.getShow);
+router.get('/files', UtilController.token, FilesController.getIndex);
+router.get('/files/:id/data', UtilController.token, FilesController.getFile);
+
+router.put('/files/:id/publish', UtilController.token, FilesController.putPublish);
+router.put('/files/:id/unpublish', UtilController.token, FilesController.putUnpublish);
 
 export default router;
